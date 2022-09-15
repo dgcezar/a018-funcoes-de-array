f7ef2209681b0644de486f4f4c066cd2d7205810
dgcezar <diego.cezar13@gmail.com>
2022-09-15 15:34:33 -0300

    exercícios feitos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,23 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+const caixaAlta = (lista) => {
+    lista.nome = objeto.nome.toUpperCase()
+    lista.profissao = objeto.profissao.toUpperCase()
+    lista.username = objeto.username.toUpperCase()
+    lista.senha = objeto.senha.toUpperCase()
+    return lista
+}
+
+console.log(caixaAlta(objeto));
+
+const textoCorrido = (lista) => {
+    return `nome: ${lista.nome}, profissao: ${lista.profissao}, username: ${lista.username}, senha: ${lista.senha}`
+}
+
+console.log(textoCorrido(objeto));
+
+const callback1 = (objeto, caixaAlta) => {
+    
+}
---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+const arrayNumeros = [123,155,543,65,34,76,5,8];
+
+const triplos = arrayNumeros.map((item) => {return item*3})
+
+const metades = arrayNumeros.map((item) => {return item/2})
+
+console.log(triplos,metades);
+
